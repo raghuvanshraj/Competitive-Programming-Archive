4fb98d8f1d6176b94bba65af9bb4065850ae79ad
Raghuvansh Raj <raghuvanshraj@hotmail.com>
2021-10-30 17:35:48 +0530

    added problems

---
+++ Codeforces/1036.cpp
@@ -0,0 +1,158 @@
+/**
+ *    author:	vulkan
+ *    created:	29.05.2021 06:31:55 PM
+**/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+#define MOD 1000000007
+#define INF 1000000000
+
+#define SET_ARR(arr,n,val) for (int i = 0; i < n; ++i) arr[i] = val
+#define SET_ARR2D(arr,n,m,val) for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) arr[i][j] = val
+#define INPUT_ARR(arr,n) for (int i = 0; i < n; ++i) cin >> arr[i];
+#define INPUT_ARR2D(arr,n,m) for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) cin >> arr[i][j];
+#define PRINT_ARR(arr) for (auto x : arr) cout << x << ' '; cout << endl
+#define PRINT_ARR2D(arr) for (auto x : arr) { for (auto y : x) cout << y << ' '; cout << endl; }
+
+#define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+
+template <typename X, typename T>
+auto vectors(X x, T a) {
+	return vector<T>(x, a);
+}
+
+template <typename X, typename Y, typename Z, typename... Zs>
+auto vectors(X x, Y y, Z z, Zs... zs) {
+	auto cont = vectors(y, z, zs...);
+	return vector<decltype(cont)>(x, cont);
+}
+
+template <typename T>
+using indexed_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T>
+using max_heap = priority_queue<T>;
+
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+template <
+    typename T,
+    typename = typename enable_if<is_arithmetic<T>::value, T>::type,
+    typename U,
+    typename = typename enable_if<is_arithmetic<U>::value, U>::type
+    >
+pair<T, U> operator+(const pair<T, U> &a, const pair<T, U> &b) {
+	return {a.first + b.first, a.second + b.second};
+}
+
+template <
+    typename T,
+    typename = typename enable_if<is_arithmetic<T>::value, T>::type,
+    typename U,
+    typename = typename enable_if<is_arithmetic<U>::value, U>::type
+    >
+pair<T, U> operator-(const pair<T, U> &a, const pair<T, U> &b) {
+	return {a.first - b.first, a.second - b.second};
+}
+
+template <
+    typename T,
+    typename = typename enable_if<is_arithmetic<T>::value, T>::type,
+    typename U,
+    typename = typename enable_if<is_arithmetic<U>::value, U>::type,
+    typename V,
+    typename = typename enable_if<is_arithmetic<V>::value, V>::type
+    >
+pair<T, U> operator*(const V &a, const pair<T, U> &b) {
+	return {a * b.first, a * b.second};
+}
+
+template <
+    typename T,
+    typename = typename enable_if<is_arithmetic<T>::value, T>::type,
+    typename U,
+    typename = typename enable_if<is_arithmetic<U>::value, U>::type,
+    typename V,
+    typename = typename enable_if<is_arithmetic<V>::value, V>::type
+    >
+pair<T, U> operator*(const pair<T, U> &b, const V &a) {
+	return {a * b.first, a * b.second};
+}
+
+template <typename T, typename U>
+istream& operator>>(istream &input, pair<T, U> &b) {
+	input >> b.first >> b.second;
+	return input;
+}
+
+template <typename T, typename U>
+ostream& operator<<(ostream &output, pair<T, U> &b) {
+	output << b.first << ' ' << b.second;
+	return output;
+}
+
+int main(int argc, char const *argv[]) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	vector<LL> a(n);
+	INPUT_ARR(a, n);
+
+	int m;
+	cin >> m;
+	vector<LL> b(m);
+	INPUT_ARR(b, m);
+
+	int i1 = 0, i2 = 0;
+	int x = 0;
+	while (i1 < n and i2 < m) {
+		int j1 = i1 + 1;
+		int j2 = i2 + 1;
+		bool flag = false;
+		while (a[i1] != b[i2]) {
+			while (a[i1] < b[i2]) {
+				if (j1 == n) {
+					flag = true;
+					break;
+				}
+				a[i1] += a[j1++];
+				x++;
+			}
+			while (a[i1] > b[i2]) {
+				if (j2 == m) {
+					flag = true;
+					break;
+				}
+				b[i2] += b[j2++];
+			}
+			if (flag) {
+				break;
+			}
+		}
+		if (flag) {
+			break;
+		}
+		i1 = j1;
+		i2 = j2;
+	}
+
+	if (i1 != n or i2 != m) {
+		cout << -1;
+	} else {
+		cout << n - x;
+	}
+
+	return 0;
+}
