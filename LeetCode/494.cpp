fba391218540cda0c25f8a79d6900842703f9cf2
Raghuvansh Raj <raghuvanshraj@hotmail.com>
2022-01-13 23:52:16 +0530

    added problems

--- LeetCode/494.cpp
+++ LeetCode/494.cpp
@@ -1,33 +1,28 @@
 /**
  *    author:	vulkan
- *    created:	23.06.2020 08:14:31 PM
+ *    created:	13.01.2022 07:39:05 PM
 **/
 #include <bits/stdc++.h>
+#define N 40001
 
 using namespace std;
 
+int idx(int x) {
+	return (x + N) % N;
+}
+
 int findTargetSumWays(vector<int> &arr, int t) {
+	int sum = accumulate(arr.begin(), arr.end(), 0);
 	int n = arr.size();
-	int s = accumulate(arr.begin(), arr.end(), 0);
-	vector<vector<int>> dp(2 * s + 1, vector<int>(n + 1));
-	for (int i = 0; i <= n; ++i) {
-		dp[0][i] = 1;
-	}
-
-	for (int j = 1; j <= n; ++j) {
-		for (int i = -s; i <= s; ++i) {
-
+	vector<vector<int>> dp(n + 1, vector<int>(N));
+	dp[0][0] = 1;
+	for (int i = 1; i <= n; ++i) {
+		for (int x = -sum; x <= sum; ++x) {
+			dp[i][idx(x)] = dp[i - 1][idx(x - arr[i - 1])] + dp[i - 1][idx(x + arr[i - 1])];
 		}
 	}
 
-	// for (int i = 0; i <= s; ++i) {
-	// 	for (int j = 0; j <= n; ++j) {
-	// 		cout << dp[i][j] << ' ';
-	// 	}
-	// 	cout << endl;
-	// }
-
-	return ((t >= 0) ? dp[t][n] : dp[2 * s + 1 + t][n]);
+	return dp[n][idx(t)];
 }
 
 int main(int argc, char const *argv[]) {
